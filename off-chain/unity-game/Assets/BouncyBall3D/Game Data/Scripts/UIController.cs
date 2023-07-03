0c7b445343e9b1e955ee74c9e9fca8a8be72e966
unknown <john.kosinski@gmail.com>
2023-07-03 16:31:16 +0700

    turn off fake sign-in

--- off-chain/unity-game/Assets/BouncyBall3D/Game Data/Scripts/UIController.cs
+++ off-chain/unity-game/Assets/BouncyBall3D/Game Data/Scripts/UIController.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     private const int SIGNING_MESSAGE_LENGTH = 32;
-    private const bool FAKE_SIGNIN = true;
+    private const bool FAKE_SIGNIN = false;
     private static string MessageToSign = "";
 
     //call to request the front end Javascript code to sign a message 
