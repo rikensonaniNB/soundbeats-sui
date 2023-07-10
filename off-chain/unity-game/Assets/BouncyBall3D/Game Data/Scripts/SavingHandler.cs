bcc88519e1e62cc5d24d40c5da01d1f73e794793
unknown <john.kosinski@gmail.com>
2023-07-11 02:14:19 +0700

    removed SavingHandler (not used)

--- off-chain/unity-game/Assets/BouncyBall3D/Game Data/Scripts/SavingHandler.cs
+++ off-chain/unity-game/Assets/BouncyBall3D/Game Data/Scripts/SavingHandler.cs
@@ -1,11 +1,9 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 
-//TODO: (LOW) is this used?
 public class SavingHandler : Singleton<SavingHandler>
 {
     public int bestScore = 0;
 
-    //TODO: (HIGH) this must not load on Awake, or else it will get the last user's high score (if this is used at all)
     protected override void Awake()
     {
         base.Awake();
@@ -22,3 +20,4 @@ public class SavingHandler : Singleton<SavingHandler>
         UserData.BestScore = bestScore;
     }
 }
+*/
