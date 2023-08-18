4e417208483d2dd7c528e0297ca4451ada2fb24b
Acer <john.kosinski@gmail.com>
2023-08-18 10:35:40 +0700

    enforce leaderboard limit

--- off-chain/api-server/src/leaderboard.ts
+++ off-chain/api-server/src/leaderboard.ts
@@ -38,11 +38,10 @@ class LeaderboardMemory implements ILeaderboard {
             this.leaderboardMap.forEach((value: number, key: string) => {
                 output.scores.push({ wallet: key, score: value });
             });
-
+            
             //sort 
-            output.scores.sort((a, b) => { return b.score - a.score });
-
-            //limit output 
+            output.scores.sort((a, b) => { return b.score - a.score }); 
+            
             if (output.scores.length > limit) {
                 output.scores = output.scores.slice(0, limit);
             }
